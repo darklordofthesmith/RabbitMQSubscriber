346da1be36d1a8ba7722962761989d1af8d7cc3b
David Smith <davesmith@gmail.com>
2018-02-12 18:12:48 -0800

    First Commit

---
+++ RabbitMQSubscriber/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQSubscriber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting RabbitMQ Subscriber!");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "SimpleMessage",
+                                     durable: true,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("Recieved a message: " + message);
+                };
+
+                channel.BasicConsume(queue: "SimpleMessage",
+                                     autoAck: true,
+                                     consumer: consumer);
+
+                Console.WriteLine("Press [enter] to exit.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
